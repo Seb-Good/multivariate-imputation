b5af59da325b5bcac12451652d4e34064882ac0c
rosie <sebi.goodfellow@gmail.com>
2020-02-07 22:30:03 -0500

    - initial commit.

--- imputation_tools.py
+++ imputation_tools.py
@@ -6,9 +6,6 @@ Expectation Maximization.
 By: Sebastian D. Goodfellow, Ph.D.
 """
 
-# Compatibility imports
-from __future__ import absolute_import, division, print_function
-
 # 3rd party imports
 import numpy as np
 import pandas as pd
